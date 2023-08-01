4fba04c554cdba3610a9ac9266fa43c19af13ea6
Matteo Sammartino <sammartino.matteo@gmail.com>
2023-08-01 16:27:43 +0100

    created adapter init script

---
+++ adapter-init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export TARGET=zcu104
+export IP_ADDRESS=192.168.1.50
+
+source venv/bin/activate
+litex_server --udp --udp-ip $(IP_ADDRESS) --udp-port $(UDP_PORT) &
+
+wait -n 
+
+exit $?
+
+
+
+
