8e094cb782d077bca79966042f4873927990c160
Matteo Sammartino <sammartino.matteo@gmail.com>
2023-08-01 16:43:08 +0100

    fix

--- adapter-init.sh
+++ adapter-init.sh
@@ -2,9 +2,10 @@
 
 export TARGET=zcu104
 export IP_ADDRESS=192.168.1.50
+export UDP_PORT=1234
 
 source venv/bin/activate
-litex_server --udp --udp-ip $(IP_ADDRESS) --udp-port $(UDP_PORT) &
+litex_server --udp --udp-ip ${IP_ADDRESS} --udp-port ${UDP_PORT} &
 
 wait -n 
 
